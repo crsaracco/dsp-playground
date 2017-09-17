7cba9811696329fda081a665cd9e5129dc57881b
Charles Saracco <crsaracco@gmail.com>
2017-09-17 03:28:52 -0400

    Clean up code a little in main.rs, add comments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+// TODO: #![deny(missing_docs)]
+
 // PortAudio:
 extern crate portaudio;
 use portaudio as pa;
@@ -11,58 +13,76 @@ use dsp::add_signals::AddSignals;
 use dsp::evaluatable::Evaluatable;
 
 // Constants:
-const CHANNELS: i32 = 2;
+const NUM_CHANNELS: i32 = 2;
 const NUM_SECONDS: i32 = 5;
 const SAMPLE_RATE: f64 = 44100.0;
 const FRAMES_PER_BUFFER: u32 = 64;
+const BUFFER_SECONDS: f64 = 0.050;  // Buffer samples for 50ms -- reduces chances of underrun
+
 
 fn main() {
+    // Create input sound generators:
     let sine_generator1 = SineGenerator::new(44100.0, 400.0, 0.3);
     let sine_generator2 = SineGenerator::new(44100.0, 400.0, 0.3);
+
+    // Negate one of the sine waves:
     let negated_sine_2 = NegateSignal::new(sine_generator2);
 
-    //let mut add_vec = vec![sine_generator1, sine_generator2];
-
+    // Add sound generators together -- result should be silence:
     let add_vec: Vec<Box<Evaluatable>> = vec![Box::new(sine_generator1), Box::new(negated_sine_2)];
-
     let add_signals = AddSignals::new(add_vec);
+
+    // Play our resulting audio:
     run(add_signals).unwrap()
 }
 
 
 fn run<E: Evaluatable + 'static>(mut generator: E) -> Result<(), pa::Error> {
-    println!("PortAudio Test: output sine wave. SR = {}, BufSize = {}", SAMPLE_RATE, FRAMES_PER_BUFFER);
-
+    // Fire up ye olde PortAudio:
     let pa = try!(pa::PortAudio::new());
 
-    let mut settings = try!(pa.default_output_stream_settings(CHANNELS, SAMPLE_RATE, FRAMES_PER_BUFFER));
-    settings.params.suggested_latency = 0.1;
+    // Set up our settings - set a buffer amount to try to reduce underruns:
+    let mut settings = try!(pa.default_output_stream_settings(NUM_CHANNELS, SAMPLE_RATE, FRAMES_PER_BUFFER));
+    settings.params.suggested_latency = BUFFER_SECONDS;
 
-    // This routine will be called by the PortAudio engine when audio is needed. It may called at
-    // interrupt level on some machines so don't do anything that could mess up the system like
-    // dynamic resource allocation or IO.
+    // This callback function will be called by PortAudio when it needs more audio samples.
+    // It may be called at interrupt level on some machines, so don't do anything that could mess
+    // up the system like dynamic resource allocation or I/O. (although doing so seems to be fine on
+    // my machine...?)
+    //
+    // The job of this callback is to fill up the buffer that PortAudio tells us to fill up.
+    // Each "frame" represents one sample for each channel that we have, so we need to put a total
+    // of (NUM_CHANNELS * frames) samples into the buffer.
+    // The samples are "interleaved" by default, so the structure of buffer looks like:
+    // [ch0_sample0, ch1_sample0, ch0_sample1, ch1_sample1, ch0_sample2, ch1_sample2, ...]
     let callback = move |pa::OutputStreamCallbackArgs { buffer, frames, .. }| {
-        let mut idx = 0;
+        let mut i = 0;
         for _ in 0..frames {
             let samples = generator.evaluate();
-            buffer[idx]   = samples.0;
-            buffer[idx+1] = samples.1;
-            idx += 2;
+            buffer[i]   = samples.0;
+            buffer[i+1] = samples.1;
+            i += 2;
         }
         pa::Continue
     };
 
+    // Now that we have the settings and the callback function set up, we can finally open the
+    // stream, through which we will actually play audio:
     let mut stream = try!(pa.open_non_blocking_stream(settings, callback));
 
+    // And now that we have the stream, we can start playing sounds!
     try!(stream.start());
 
+    // We're using PortAudio in non-blocking mode, so execution will fall through immedately.
+    // Let's sleep for NUM_SECONDS to let PortAudio play sounds for a bit.
+    // Note that pa.sleep() will sleep for *at least* NUM_SECONDS, but probably more -- don't depend
+    // on pa.sleep() for musical timings or anything like that.
     println!("Play for {} seconds.", NUM_SECONDS);
     pa.sleep(NUM_SECONDS * 1_000);
 
+    // We're done playing, gracefully shut down the stream:
     try!(stream.stop());
     try!(stream.close());
-
-    println!("Test finished.");
 
     Ok(())
 }
